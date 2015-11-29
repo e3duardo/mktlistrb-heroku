650de7219e12bfeea3867f852bff0ea705c69968
e3duardo <e3duardo@live.com>
2015-11-29 04:51:48 -0200

    no primary version

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,14 @@
 #   5.times do |i|
 #     Product.create(name: "Product ##{i}", description: "A product.")
 #   end
-35.times do |i|
-  Store.create(name: "Loja ##{i}", description: "Uma loja.")
-end
 
-20.times do |i|
-  Product.create(name: "Produto ##{i}", alias: "Um produto.")
-end
+@store = Store.create(name: 'Fluminense', description: 'Loja perto da Faculdade Redentor', latitude: '-21.1963157', longitude: '-41.906837,19')
+
+@produto1 = Product.create(name: "Arroz", alias: "Tio zé")
+@produto2 = Product.create(name: "Feijão", alias: "Tio joão")
+
+
+Price.create(store_id: @produto1.id, product_id: @produto1.id, price: 9.75)
+Price.create(store_id: @produto2.id, product_id: @produto2.id, price: 4.32)
+
+
