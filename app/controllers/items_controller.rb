b2da24d23bff82b0d429c0e439d400ca79a21138
e3duardo <e3duardo@live.com>
2015-12-01 21:51:32 -0200

    fix bug not adding item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,36 +10,34 @@ class ItemsController < ApplicationController
   end
 
   def create
-    if Item.find_by_product(params[:item][:product]).nil?
-      redirect_to :back
+    if not Item.find_by_product(params[:item][:product]).nil?
       #TODO: criar validacao na view quando o produto já esta na lista
     else
       @item = Item.create(list_params)
-      redirect_to :back
     end
-
-    end
-
-    def update
-      @item = Item.find(params[:id])
-
-      @item.update(list_params)
-
-      @item.total = params[:item][:amount].to_f * params[:item][:price].to_f
-
-      @item.update(list_params)
-      redirect_to :back
-    end
-
-    def destroy
-      item = Item.find(params[:id])
-      item.destroy
-      redirect_to :back
-    end
-
-
-    private
-    def list_params
-      params.require(:item).permit(:product, :list_id, :price, :amount, :total)
-    end
+    redirect_to :back
   end
+
+  def update
+    @item = Item.find(params[:id])
+
+    @item.update(list_params)
+
+    @item.total = params[:item][:amount].to_f * params[:item][:price].to_f
+
+    @item.update(list_params)
+    redirect_to :back
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to :back
+  end
+
+
+  private
+  def list_params
+    params.require(:item).permit(:product, :list_id, :price, :amount, :total)
+  end
+end
